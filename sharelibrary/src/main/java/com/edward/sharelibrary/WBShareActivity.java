5e7f958a1b0b6ca80e31ff8e51854d456c63397d
Zhang Yue <124055429@qq.com>
2016-01-13 19:03:34 +0800

    完成分享集成,即将修改包名,测试分享

---
+++ sharelibrary/src/main/java/com/edward/sharelibrary/WBShareActivity.java
@@ -0,0 +1,9 @@
+package com.edward.sharelibrary;
+
+import com.umeng.socialize.media.WBShareCallBackActivity;
+
+/**
+ * Created by wangfei on 15/12/3.
+ */
+public class WBShareActivity extends WBShareCallBackActivity{
+}
