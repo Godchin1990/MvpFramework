a6615484b37e0981862a76f05b376dfa11385916
Zhang Yue <124055429@qq.com>
2016-01-14 14:14:41 +0800

    我的页面画完了,采用命令设计模式和外观模式,填充item条目数据

---
+++ app/src/main/java/com/ziyou/tourGuide/command/view/MeItemCommand.java
@@ -0,0 +1,69 @@
+package com.ziyou.tourGuide.command.view;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ziyou.tourGuide.R;
+import com.ziyou.tourGuide.command.base.Command;
+
+/**
+ * Created by Edward on 16/1/14.
+ */
+public class MeItemCommand implements Command {
+
+    public static final String PARAM_TITLE = "title";
+    public static final String PARAM_ICON = "iconId";
+
+    private View itemView;
+    private Bundle itemBundle;
+
+    private ViewCallBack viewCallBack;
+
+    private final String title;
+    private final int iconId;
+
+    public MeItemCommand(View itemView, Bundle itemBundle) {
+        this.itemView = itemView;
+        this.itemBundle = itemBundle;
+
+        title = itemBundle.getString(PARAM_TITLE);
+        iconId = itemBundle.getInt(PARAM_ICON);
+    }
+
+    public MeItemCommand(View itemView, Bundle itemBundle,ViewCallBack viewCallBack) {
+        this.itemView = itemView;
+        this.itemBundle = itemBundle;
+        this.viewCallBack = viewCallBack;
+
+        title = itemBundle.getString(PARAM_TITLE);
+        iconId = itemBundle.getInt(PARAM_ICON);
+    }
+
+    /**
+     * 设置获得layout的回调函数callback
+     * @param viewCallBack
+     */
+    public void setViewCallBack(ViewCallBack viewCallBack) {
+        this.viewCallBack = viewCallBack;
+    }
+
+    @Override
+    public void execute() {
+        ImageView itemIcon = (ImageView) itemView.findViewById(R.id.item_icon);
+        TextView itemText = (TextView) itemView.findViewById(R.id.item_text);
+        itemIcon.setImageDrawable(itemView.getResources().getDrawable(iconId));
+        itemText.setText(title);
+
+        if(viewCallBack !=null){
+            ViewGroup viewGroup = (ViewGroup) itemView.findViewById(R.id.item_layout);
+            viewGroup.addView(viewCallBack.getInflateView());
+        }
+    }
+
+    public interface ViewCallBack{
+        View getInflateView();
+    }
+}
--- app/src/main/java/com/ziyou/tourGuide/view/MeView.java
+++ app/src/main/java/com/ziyou/tourGuide/view/MeView.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.ziyou.tourGuide.R;
+import com.ziyou.tourGuide.command.view.InitMeItemCommand;
 import com.ziyou.tourGuide.view.base.TitleBarContentView;
 import com.ziyou.tourGuide.view.interfaze.IMeView;
 import com.ziyou.tourGuide.widget.MyActionBar;
-import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,10 +22,25 @@ import butterknife.ButterKnife;
  */
 public class MeView extends TitleBarContentView implements IMeView {
 
+
+
     @Bind(R.id.me_infomation)
     ViewGroup me_infomation;
     @Bind(R.id.riv_user_avatar)
     SimpleDraweeView riv_user_avatar;
+    @Bind(R.id.my_message)
+    View my_message;
+    @Bind(R.id.guider_area)
+    View guider_area;
+    @Bind(R.id.my_tour)
+    View my_tour;
+    @Bind(R.id.my_wallet)
+    View my_wallet;
+    @Bind(R.id.customer_service)
+    View customer_service;
+    @Bind(R.id.setting)
+    View setting;
+
     public MeView(Context context) {
         super(context);
     }
@@ -30,7 +49,22 @@ public class MeView extends TitleBarContentView implements IMeView {
     public View setContentView() {
         View view = View.inflate(getContext(), R.layout.fragment_me,null);
         ButterKnife.bind(this,view);
+
+        initMeItemList();
         return view;
+    }
+
+    private void initMeItemList() {
+        List<View> list = new ArrayList<>();
+        list.add(my_message);
+        list.add(guider_area);
+        list.add(my_tour);
+        list.add(my_wallet);
+        list.add(customer_service);
+        list.add(setting);
+
+        InitMeItemCommand initMeItemCommand = new InitMeItemCommand(getContext(),list);
+        initMeItemCommand.execute();
     }
 
     @Override
